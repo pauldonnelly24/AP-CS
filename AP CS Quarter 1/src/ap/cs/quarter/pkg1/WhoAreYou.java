ca51fb5a48abfc92e20a2cd66eab7b70f78f86f8
matthewGonzalgo <31327189+matthewGonzalgo@users.noreply.github.com>
2017-08-28 21:33:44 -0400

    Add files via upload

---
+++ AP CS Quarter 1/src/ap/cs/quarter/pkg1/WhoAreYou.java
@@ -0,0 +1,43 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ap.cs.quarter.pkg1;
+import java.util.Scanner;
+/**
+ *
+ * @author matthew.gonzalgo
+ */
+public class WhoAreYou {
+
+public static void main (String[] args) {
+String fullName, gender, title;
+String m = "male";
+String f = "female";
+int age, height; 
+
+Scanner in = new Scanner(System.in);
+
+System.out.println("What is your gender? Enter either 'male' or 'female'");
+gender = in.nextLine();
+if (gender.equalsIgnoreCase(m) == true){
+    title = "Mr.";
+}else if(gender.equalsIgnoreCase(f) == true){
+    title = "Mrs.";
+}else{
+    System.out.println("You either didn't enter 'male' or 'female correctly, or you somehow identify as a different gender...");
+}
+
+System.out.println("What is your full name?");
+fullName = in.nextLine();
+
+System.out.println("How old are you?");
+age = in.nextInt();
+
+System.out.println("How tall are you in inches?");
+height = in.nextInt();
+
+System.out.println("Hello " + title + " " fullName + "!");
+System.out.println("You are a " + age + " year old " gender + " who is " + height + " inches tall!");
+}
+}
